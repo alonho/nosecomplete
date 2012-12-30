aaad954e062c737b760bf4105e551afd6af3e603
Alon Horev <alonho@gmail.com>
2012-12-30 15:18:16 +0200

    inc version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nosecomplete',
-      version='0.0.3',
+      version='0.0.4',
       description='nosecomplete is a nose plugin used for completing test names from the command line, it supports both python 2 and 3.',
       author='Alon Horev',
       author_email='alon@horev.net',
