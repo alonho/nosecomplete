db2b1b3d756b14fa62cc9e01cc16720e5e2dd3a6
Alon Horev <alonho@gmail.com>
2013-01-11 13:01:09 +0200

    add tests

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,10 @@ FIXTURES = {
 
 class _BaseTestFinderTestCase(unittest.TestCase):
 
+    def test_complete(self):
+        # this prints to the screen.. just make sure the code's not broken
+        nosecomplete.complete(self.finder, FIXTURES['basic'])
+    
     def test_basic(self):
         actual = list(self.finder.get_module_tests(FIXTURES['basic']))
         expected = [
@@ -16,9 +20,42 @@ class _BaseTestFinderTestCase(unittest.TestCase):
             'test_blue',
         ]
         self.assertEqual(set(actual), set(expected))
+        
+    def _assert_complete(self, thing, options):
+        self.assertEqual(set(nosecomplete._complete(self.finder, thing)),
+                         set(options))
 
+    def test_dir_prefix(self):
+        self._assert_complete('tests/fixtures', ['/'])
+        
+    def test_dir(self):
+        try:
+            options = ['fixtures/', '__init__.py:', 'tests.py:']
+            self._assert_complete('tests/', options)
+        except AssertionError:
+            options.append('__pycache__/') # py3 leaves these around
+            self._assert_complete('tests/', options)
+                
+    def test_partial_filename(self):
+        self._assert_complete('tests/fixtures/ba', ['sic.py:'])
+        
+    def test_filename(self):
+        self._assert_complete('tests/fixtures/basic.py', [':'])
+
+    def test_partial_case(self):
+        self._assert_complete('tests/fixtures/basic.py:test', ['_red', '_blue'])
+
+    def test_partial_class(self):
+        self._assert_complete('tests/fixtures/basic.py:AwesomeTestCase', ['.'])
+
+    def test_partial_method(self):
+        self._assert_complete('tests/fixtures/basic.py:AwesomeTestCase.',
+                              ['test_green',
+                               'test_yellow'])
+        
 class NoseTestFinderTestCase(_BaseTestFinderTestCase):
     finder = nosecomplete.NoseTestFinder()
     
 class PythonTestFinderTestCase(_BaseTestFinderTestCase):
     finder = nosecomplete.PythonTestFinder()
+
